85d0bb1b080617cd33c1b845216bf5c229d6a7a3
Ryan Breen <ryan@breen.xyz>
2024-12-08 21:39:52 -0500

    d8p2

--- src/day08/mod.rs
+++ src/day08/mod.rs
@@ -15,11 +15,15 @@ pub fn run(example: bool) -> anyhow::Result<()> {
 
 fn p1(input: &str) {
     let board = parse(input);
-    let antinodes = find_antinodes(&board);
+    let antinodes = find_antinodes(&board, false);
     println!("P1: {}", antinodes.len());
 }
 
-fn p2(input: &str) {}
+fn p2(input: &str) {
+    let board = parse(input);
+    let antinodes = find_antinodes(&board, true);
+    println!("P1: {}", antinodes.len());
+}
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Coord {
@@ -45,12 +49,16 @@ fn find_nodes(board: &[Vec<char>]) -> HashMap<char, HashSet<Coord>> {
     nodes
 }
 
-fn find_antinodes(board: &[Vec<char>]) -> HashSet<Coord> {
+fn find_antinodes(board: &[Vec<char>], resonance: bool) -> HashSet<Coord> {
     let nodes = find_nodes(board);
     let mut antinodes = HashSet::new();
     for (_node, coords) in nodes {
         for perms in coords.into_iter().permutations(2) {
-            let lan = line_antinodes(board, perms[0], perms[1]);
+            let lan = if resonance {
+                resonant_line_antinodes(board, perms[0], perms[1])
+            } else {
+                line_antinodes(board, perms[0], perms[1])
+            };
             antinodes.extend(lan);
         }
     }
@@ -63,42 +71,77 @@ fn line_antinodes(board: &[Vec<char>], a: Coord, b: Coord) -> Vec<Coord> {
     {
         let x = a.x as isize + run;
         let y = a.y as isize + rise;
-        if x >= 0 && y >= 0 {
-            let x = x as usize;
-            let y = y as usize;
-            if y < board.len() && x < board[0].len() {
-                antinodes.push(Coord { x, y });
-            }
+        if is_on_board(board, x, y) {
+            antinodes.push(Coord {
+                x: x as usize,
+                y: y as usize,
+            });
         }
     }
 
     #[allow(clippy::comparison_chain)]
-    let rise = if rise < 0 {
-        rise.abs()
-    } else if rise > 0 {
-        0 - rise
-    } else {
-        rise
-    };
+    let rise = flip_sign(rise);
 
     #[allow(clippy::comparison_chain)]
-    let run = if run < 0 {
-        run.abs()
-    } else if run > 0 {
-        0 - run
-    } else {
-        run
-    };
+    let run = flip_sign(run);
 
     {
         let x = b.x as isize + run;
         let y = b.y as isize + rise;
-        if x >= 0 && y >= 0 {
-            let x = x as usize;
-            let y = y as usize;
-            if board.get(y).and_then(|l| l.get(x)).is_some() {
-                antinodes.push(Coord { x, y });
-            }
+        if is_on_board(board, x, y) {
+            antinodes.push(Coord {
+                x: x as usize,
+                y: y as usize,
+            });
+        }
+    }
+    antinodes
+}
+
+fn resonant_line_antinodes(board: &[Vec<char>], a: Coord, b: Coord) -> Vec<Coord> {
+    let mut antinodes = Vec::new();
+
+    // All points in the line are now antinodes becuase of resonance, which includes the antenna
+    // points.
+    antinodes.extend([a, b]);
+
+    let (rise, run) = slope(a, b);
+
+    let mut a = a;
+    loop {
+        let x = a.x as isize + run;
+        let y = a.y as isize + rise;
+        if is_on_board(board, x, y) {
+            let coord = Coord {
+                x: x as usize,
+                y: y as usize,
+            };
+            antinodes.push(coord);
+            a = coord;
+        } else {
+            break;
+        }
+    }
+
+    #[allow(clippy::comparison_chain)]
+    let rise = flip_sign(rise);
+
+    #[allow(clippy::comparison_chain)]
+    let run = flip_sign(run);
+
+    let mut b = b;
+    loop {
+        let x = b.x as isize + run;
+        let y = b.y as isize + rise;
+        if is_on_board(board, x, y) {
+            let coord = Coord {
+                x: x as usize,
+                y: y as usize,
+            };
+            antinodes.push(coord);
+            b = coord;
+        } else {
+            break;
         }
     }
     antinodes
@@ -106,6 +149,27 @@ fn line_antinodes(board: &[Vec<char>], a: Coord, b: Coord) -> Vec<Coord> {
 
 fn slope(a: Coord, b: Coord) -> (isize, isize) {
     (a.y as isize - b.y as isize, a.x as isize - b.x as isize)
+}
+
+fn is_on_board(board: &[Vec<char>], x: isize, y: isize) -> bool {
+    if x < 0 || y < 0 {
+        return false;
+    }
+
+    let x = x as usize;
+    let y = y as usize;
+
+    y < board.len() && x < board[0].len()
+}
+
+fn flip_sign(n: isize) -> isize {
+    if n < 0 {
+        n.abs()
+    } else if n > 0 {
+        0 - n
+    } else {
+        n
+    }
 }
 
 #[cfg(test)]
