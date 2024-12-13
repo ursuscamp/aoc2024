d1f974eebd7974c80c39c7916c15bbb9331d703f
Ryan Breen <ryan@breen.xyz>
2024-12-13 17:18:28 -0500

    d13p2

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,4 +1,4 @@
-use std::{cell::OnceCell, sync::OnceLock};
+use std::sync::OnceLock;
 
 use itertools::Itertools;
 use regex::Regex;
@@ -15,18 +15,22 @@ pub fn run(example: bool) -> anyhow::Result<()> {
 }
 
 fn p1(input: &str) {
-    let machines = parse(input);
-    let cost: usize = machines.iter().flat_map(Machine::solve).sum();
+    let machines = parse(input, 0);
+    let cost: isize = machines.iter().flat_map(Machine::solve).sum();
     println!("P1: {cost:?}");
 }
 
-fn p2(input: &str) {}
+fn p2(input: &str) {
+    let machines = parse(input, 10000000000000);
+    let cost: isize = machines.iter().flat_map(Machine::solve).sum();
+    println!("P2: {cost:?}");
+}
 
-fn parse(input: &str) -> Vec<Machine> {
+fn parse(input: &str, correction: isize) -> Vec<Machine> {
     input
         .lines()
         .tuples()
-        .map(|(a, b, p, _)| Machine::parse(a, b, p))
+        .map(|(a, b, p, _)| Machine::parse(a, b, p, correction))
         .collect()
 }
 
@@ -36,13 +40,13 @@ static PRIZE: OnceLock<Regex> = OnceLock::new();
 
 #[derive(Debug, Clone, Copy)]
 struct Machine {
-    a: (usize, usize),
-    b: (usize, usize),
-    prize: (usize, usize),
+    a: (isize, isize),
+    b: (isize, isize),
+    prize: (isize, isize),
 }
 
 impl Machine {
-    fn parse(button_a: &str, button_b: &str, prize: &str) -> Machine {
+    fn parse(button_a: &str, button_b: &str, prize: &str, correction: isize) -> Machine {
         let regex_a =
             BUTTON_A.get_or_init(|| Regex::new(r#"Button A: X\+(\d+), Y\+(\d+)$"#).unwrap());
         let regex_b =
@@ -56,25 +60,22 @@ impl Machine {
         let b = (caps[1].parse().unwrap(), caps[2].parse().unwrap());
 
         let caps = regex_p.captures(prize).expect("prize");
-        let prize = (caps[1].parse().unwrap(), caps[2].parse().unwrap());
+        let prize = (
+            caps[1].parse::<isize>().unwrap() + correction,
+            caps[2].parse::<isize>().unwrap() + correction,
+        );
 
         Machine { a, b, prize }
     }
 
-    fn solve(&self) -> Option<usize> {
-        (0usize..100)
-            .flat_map(|a| (0usize..100).map(move |b| (a, b)))
-            .flat_map(|(a, b)| {
-                let cost = a * 3 + b;
-                let x = self.a.0 * a + self.b.0 * b;
-                let y = self.a.1 * a + self.b.1 * b;
-                if (x, y) == self.prize {
-                    // println!("Prize found: {x},{y} => {cost}");
-                    Some(cost)
-                } else {
-                    None
-                }
-            })
-            .min()
+    fn solve(&self) -> Option<isize> {
+        let b = (self.a.0 * self.prize.1 - self.a.1 * self.prize.0)
+            / (self.a.0 * self.b.1 - self.a.1 * self.b.0);
+        let a = (self.prize.0 - self.b.0 * b) / self.a.0;
+        let f = (self.a.0 * a + self.b.0 * b, self.a.1 * a + self.b.1 * b);
+        if f != self.prize {
+            return None;
+        }
+        Some(3 * a + b)
     }
 }
