786e59df893e55e39eb6e19388e0ed08d1fb8566
Ryan Breen <ryan@breen.xyz>
2024-12-17 12:25:14 -0500

    d16p2
    d16p2
    
    d16p2
    
    d16p2
    
    d16p2
    
    d16p2
    
    d16p2 - player id
    
    d16p2 - global visited
    
    d16p2 - performant enough, missing a few tiles for some reason
    
    d16p2

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -1,6 +1,10 @@
-use std::collections::{HashMap, VecDeque};
+use std::{collections::BinaryHeap, sync::atomic::AtomicUsize};
+
+use rustc_hash::{FxHashMap, FxHashSet};
 
 use crate::utils::{input, Vec2};
+
+static PLAYER_ID: AtomicUsize = AtomicUsize::new(0);
 
 pub fn run(example: bool) -> anyhow::Result<()> {
     println!("Day 16");
@@ -13,11 +17,12 @@ pub fn run(example: bool) -> anyhow::Result<()> {
 
 fn p1(input: &str) {
     let board = parse(input);
-    let score = board.new_traverse();
+    let (score, count) = board.new_traverse();
     println!("P1: {score}");
+    println!("P2: {count}");
 }
 
-fn p2(input: &str) {}
+fn p2(_input: &str) {}
 
 fn parse(input: &str) -> Board {
     let mut map = Vec::new();
@@ -45,12 +50,7 @@ fn parse(input: &str) -> Board {
     Board {
         map,
         end,
-        player: Player {
-            finished: false,
-            pos,
-            dir: Dir::East,
-            score: 0,
-        },
+        player: Player::new(pos),
     }
 }
 
@@ -62,48 +62,87 @@ struct Board {
 }
 
 impl Board {
-    fn new_traverse(&self) -> usize {
-        let mut scores = HashMap::new();
-        scores.insert(self.player.pos, 0usize);
-        let mut queue = VecDeque::from([self.player]);
-        let (a, b) = self.player.cross(self);
-        queue.extend([a, b].into_iter().flatten());
+    fn new_traverse(&self) -> (usize, usize) {
+        let mut scores = FxHashMap::default();
+        scores.insert((self.player.pos, self.player.dir), 0usize);
 
-        while let Some(mut player) = queue.pop_front() {
-            while !player.finished {
+        let mut queue = BinaryHeap::from([self.player.clone()]);
+        queue.extend(self.player.cross());
+
+        let mut visited: FxHashMap<usize, FxHashSet<Vec2>> = FxHashMap::default();
+        visited
+            .entry(self.player.id)
+            .or_default()
+            .insert(self.player.pos);
+        let mut happy_player: Vec<Player> = Vec::new();
+
+        while let Some(mut player) = queue.pop() {
+            loop {
                 // Move the player forward
-                let new_players = player.advance(self);
+                let new_players = player.advance();
 
-                // Get the score for the current cell
-                let s = scores.entry(player.pos).or_insert(usize::MAX);
+                visited.entry(player.id).or_default().insert(player.pos);
 
-                // If we already have a lower score for this cell then we can skip it
-                if player.score > *s {
+                let score = scores.entry((player.pos, player.dir)).or_insert(usize::MAX);
+
+                if self.is_wall(player.pos) {
+                    visited.remove(&player.id);
                     break;
                 }
 
-                // Store the score for the current cell
-                *s = player.score.min(*s);
+                if player.score > *score {
+                    visited.remove(&player.id);
+                    break;
+                }
 
-                // If we have reached the end cell, then stop
-                if player.pos == self.end {
+                *score = player.score.min(*score);
+
+                if self.is_end(player.pos) {
+                    happy_player.push(player);
                     break;
                 }
 
                 // Keep evaluating next players
-                queue.extend(new_players.into_iter().flatten());
+                let nv = visited.get(&player.id).unwrap().clone();
+                visited
+                    .entry(new_players[0].id)
+                    .or_insert_with(|| nv.clone());
+                visited.entry(new_players[1].id).or_insert_with(|| nv);
+                queue.extend(new_players);
             }
         }
 
-        *scores.get(&self.end).unwrap()
+        let lowest_score = scores
+            .into_iter()
+            .filter_map(|((p, _d), v)| if p == self.end { Some(v) } else { None })
+            .min()
+            .unwrap();
+        let mut success_set = happy_player
+            .into_iter()
+            .filter_map(|player| {
+                if player.score <= lowest_score {
+                    Some(player.id)
+                } else {
+                    None
+                }
+            })
+            .flat_map(|player_id| &visited[&player_id])
+            .copied()
+            .collect::<FxHashSet<_>>();
+        success_set.insert(self.player.pos);
+        (lowest_score, success_set.len())
     }
 
     #[allow(dead_code)]
-    fn print_debug(&self) {
+    fn print_debug(&self, markers: Option<FxHashSet<Vec2>>) {
+        let markers = markers.unwrap_or_default();
         for (y, line) in self.map.iter().enumerate() {
+            print!("{y:>3}: ");
             for (x, tile) in line.iter().enumerate() {
                 let pos: Vec2 = (x as isize, y as isize).into();
-                if pos == self.player.pos {
+                if markers.contains(&pos) {
+                    print!("O");
+                } else if pos == self.player.pos {
                     print!("S");
                 } else if pos == self.end {
                     print!("E");
@@ -121,21 +160,20 @@ impl Board {
             .and_then(|line| line.get(pos.x as usize))
             .copied()
     }
+
+    fn is_wall(&self, pos: Vec2) -> bool {
+        self.get(pos) == Some(Tile::Wall)
+    }
+
+    fn is_end(&self, pos: Vec2) -> bool {
+        pos == self.end
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 enum Tile {
     Floor,
     Wall,
-}
-
-impl Tile {
-    fn is_traversable(&self) -> bool {
-        match self {
-            Tile::Floor => true,
-            Tile::Wall => false,
-        }
-    }
 }
 
 impl std::fmt::Display for Tile {
@@ -187,109 +225,103 @@ impl Dir {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq)]
 struct Player {
-    finished: bool,
+    id: usize,
     pos: Vec2,
     dir: Dir,
     score: usize,
 }
 
 impl Player {
-    fn advance(&mut self, board: &Board) -> [Option<Player>; 2] {
+    fn new(pos: Vec2) -> Player {
+        Player {
+            id: PLAYER_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed),
+            pos,
+            dir: Dir::East,
+            score: 0,
+        }
+    }
+
+    fn advance(&mut self) -> [Player; 2] {
+        self.go(self.dir);
+
+        self.cross()
+    }
+
+    fn east_west_cross(&self) -> [Player; 2] {
+        let east = {
+            let mut east = self.clone();
+            east.turn(Dir::East);
+            east
+        };
+        let west = {
+            let mut west = self.clone();
+            west.turn(Dir::West);
+            west
+        };
+
+        [east, west]
+    }
+
+    fn north_south_cross(&self) -> [Player; 2] {
+        let north = {
+            let mut north = self.clone();
+            north.turn(Dir::North);
+            north
+        };
+        let south = {
+            let mut south = self.clone();
+            south.turn(Dir::South);
+            south
+        };
+
+        [north, south]
+    }
+
+    fn turn(&mut self, dir: Dir) {
+        self.score += self.dir.to(dir);
+        self.dir = dir;
+    }
+
+    fn go(&mut self, dir: Dir) {
+        self.turn(dir);
+        self.pos = match dir {
+            Dir::North => self.pos.up(),
+            Dir::South => self.pos.down(),
+            Dir::East => self.pos.right(),
+            Dir::West => self.pos.left(),
+        };
+        self.score += 1;
+    }
+
+    fn cross(&self) -> [Player; 2] {
         match self.dir {
-            Dir::North => self.north(),
-            Dir::South => self.south(),
-            Dir::East => self.east(),
-            Dir::West => self.west(),
-        }
-
-        if self.pos == board.end {
-            self.finished = true;
-            return [None, None];
-        }
-
-        let tile = board.get(self.pos).unwrap();
-        if tile.is_traversable() {
-            let (a, b) = self.cross(board);
-            [a, b]
-        } else {
-            self.finished = true;
-            [None, None]
+            Dir::North | Dir::South => self.east_west_cross(),
+            Dir::East | Dir::West => self.north_south_cross(),
         }
     }
+}
 
-    fn east_west_cross(&self, board: &Board) -> (Option<Player>, Option<Player>) {
-        let east = board
-            .get(self.pos.right())
-            .filter(Tile::is_traversable)
-            .map(|_| {
-                let mut east = *self;
-                east.east();
-                east
-            });
-
-        let west = board
-            .get(self.pos.left())
-            .filter(Tile::is_traversable)
-            .map(|_| {
-                let mut west = *self;
-                west.west();
-                west
-            });
-
-        (east, west)
+impl PartialOrd for Player {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
     }
+}
 
-    fn north_south_cross(&self, board: &Board) -> (Option<Player>, Option<Player>) {
-        let north = board
-            .get(self.pos.up())
-            .filter(Tile::is_traversable)
-            .map(|_| {
-                let mut north = *self;
-                north.north();
-                north
-            });
-        let south = board
-            .get(self.pos.down())
-            .filter(Tile::is_traversable)
-            .map(|_| {
-                let mut south = *self;
-                south.south();
-                south
-            });
-
-        (north, south)
+impl Ord for Player {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.score.cmp(&other.score)
     }
+}
 
-    fn north(&mut self) {
-        self.pos = self.pos.up();
-        self.score += self.dir.to(Dir::North) + 1;
-        self.dir = Dir::North;
-    }
-
-    fn south(&mut self) {
-        self.pos = self.pos.down();
-        self.score += self.dir.to(Dir::South) + 1;
-        self.dir = Dir::South;
-    }
-
-    fn west(&mut self) {
-        self.pos = self.pos.left();
-        self.score += self.dir.to(Dir::West) + 1;
-        self.dir = Dir::West;
-    }
-
-    fn east(&mut self) {
-        self.pos = self.pos.right();
-        self.score += self.dir.to(Dir::East) + 1;
-        self.dir = Dir::East;
-    }
-
-    fn cross(&self, board: &Board) -> (Option<Player>, Option<Player>) {
-        match self.dir {
-            Dir::North | Dir::South => self.east_west_cross(board),
-            Dir::East | Dir::West => self.north_south_cross(board),
+impl Clone for Player {
+    fn clone(&self) -> Self {
+        Self {
+            id: PLAYER_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed),
+            pos: self.pos,
+            dir: self.dir,
+            score: self.score,
         }
     }
 }
